d63bb3443c79b0de9537c73ce5a54b3616b51bf0
David Mateus <81383566+matdavid7272@users.noreply.github.com>
2025-04-01 15:41:26 -0400

    Update ArithmeticOperationTest.java

--- src/test/java/soen/tutorial/ArithmeticOperationTest.java
+++ src/test/java/soen/tutorial/ArithmeticOperationTest.java
@@ -3,7 +3,7 @@ package soen.tutorial;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-
+import static org.junit.Assert.fail;
 public class ArithmeticOperationTest {
 
 
@@ -14,7 +14,7 @@ public class ArithmeticOperationTest {
 		ArithmeticOperation operations = new ArithmeticOperation();
 		Integer actual = operations.addOrSub(2, 6);
 		Integer expected = 8;
-		assertEquals(expected, actual);
+		fail("Must fail");
 
 	}
     // @Test
